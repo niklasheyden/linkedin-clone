ed34377750c56f15a2589e88d737b46b5c462cc6
Niklas von Heyden <niklas.heyden@me.com>
2021-05-12 19:04:40 +0200

    setup user slice and add login page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
+import { selectUser } from './features/userSlice';
+import Login from './Login';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 
 function App() {
+  const user = useSelector(selectUser)
+
+
   return (
     <div className="app">
 
-      {/* Header */}
       <Header />
 
-      {/* App Body */}
-      <div className="app_body">
-        <Sidebar />
-        <Feed />
-        {/* Widgets */}
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app_body">
+          <Sidebar />
+          <Feed />
+          {/* Widgets */}
+        </div>
+      )}
+
+
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -16,8 +16,8 @@ const recentItem = (topic) => (
       <div className="sidebar_top">
         <img src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fD88MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHw%3D&w=1000&q=80" alt="" />
         <Avatar className="sidebar_avatar"/>
-        <h2>Sonny Sangha</h2>
-        <h4>sonny.sangha@gmail.com</h4>
+        <h2>Niklas Heyden</h2>
+        <h4>niklas.heyden@me.com</h4>
       </div>
 
       <div className="sidebar_stats">
